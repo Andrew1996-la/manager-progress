b205f8f071f31aaaac070d5daeac8674f8bf9987
Andrew1996-la <asoborev@mail.ru>
2022-06-19 22:00:01 +0300

    create header

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <img src="" alt="logo" />
+    </div>
+    <h1 class="name_app">Live consciously</h1>
+    <button>btn</button>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
